a0016026155a7a3ed04cf8b02c7d7235840ab301
Kevin Lu <lukevin13@gmail.com>
2024-09-29 16:12:43 -0400

    change deploy master branch name

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:lukevin13/unoriginal-games.git main:gh-pages
+git push -f git@github.com:lukevin13/unoriginal-games.git master:gh-pages
 
 cd -
