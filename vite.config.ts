01a12012cae855b5e89285fee3ca16ec3457a2fc
Kevin Lu <lukevin13@gmail.com>
2024-09-29 16:49:07 -0400

    fix vite config for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        app: './index.html'
+      }
+    }
   }
 })
